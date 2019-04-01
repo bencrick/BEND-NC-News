2860cbbef54f4b83d8ba5515a9bb0a5b5ee29b55
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-04-01 16:36:58 +0100

    fix article_id data type in create_comment_table

--- db/migrations/20190401115459_create_comment_table.js
+++ db/migrations/20190401115459_create_comment_table.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('comments', commentTable => {
     commentTable.increments('comment_id').primary();
     commentTable.string('author').references('users.username');
-    commentTable.string('article_id').references('articles.article_id');
+    commentTable.integer('article_id').references('articles.article_id');
     commentTable.integer('votes').defaultTo(0);
     commentTable.timestamp('created_at').defaultTo(knex.fn.now());
     commentTable.string('body');
