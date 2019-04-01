1538bbfce9096e8a9fad9f2ef52bc9f61270faa3
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-04-01 11:44:13 +0100

    set up index.js files in db/data

--- db/data/dev-data/index.js
+++ db/data/dev-data/index.js
@@ -1,1 +1,7 @@
 // require in and export out all dev data
+const articles = require('./articles');
+const comments = require('./comments');
+const topics = require('./topics');
+const users = require('./users');
+
+module.exports = { articles, comments, topics, users };
