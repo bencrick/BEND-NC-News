30475b113c7feef09b20ec19194f5634933978ef
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-04-02 11:57:13 +0100

    add article router to api router

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,8 +1,13 @@
 const apiRouter = require('express').Router();
 const { methodNotAllowed } = require('../errors');
+const articlesRouter = require('./articles-router')
 
 apiRouter
   .route('/')
+
+apiRouter.use('/articles', articlesRouter)
+
+apiRouter
   .get((req, res) => res.send({ ok: true }))
   .all(methodNotAllowed);
 
