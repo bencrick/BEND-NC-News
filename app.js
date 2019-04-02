0264537361345ed8010cb67667373f47cb11022c
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-04-02 12:48:27 +0100

    comments unused defaults from project generation

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const apiRouter = require('./routes/api-router');
-const { routeNotFound, handle500 } = require('./errors');
+//const { routeNotFound, handle500 } = require('./errors');
 
 const app = express();
 
@@ -8,8 +8,8 @@ app.use(express.json());
 
 app.use('/api', apiRouter);
 
-app.all('/*', routeNotFound);
+//app.all('/*', routeNotFound);
 
-app.use(handle500);
+//app.use(handle500);
 
 module.exports = app;
