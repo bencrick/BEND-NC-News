9656ef4a96733b8e0013c668487687717cee5ec4
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-04-04 12:33:09 +0100

    heroku setup

--- db/data/index.js
+++ db/data/index.js
@@ -2,10 +2,12 @@ const ENV = process.env.NODE_ENV || 'development';
 
 const development = require('./dev-data');
 const test = require('./test-data');
+const production = require('./dev-data');
 
 const data = {
   development,
   test,
+  production
 };
 
 module.exports = data[ENV];
